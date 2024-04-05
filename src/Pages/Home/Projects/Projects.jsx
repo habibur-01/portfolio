2696d30ea2695c2968f20d92c321c8abe15d04c9
habibur-01 <h.r.habibur08@gmail.com>
2024-04-05 22:57:49 +0600

    update project section

--- src/Pages/Home/Projects/Projects.jsx
+++ src/Pages/Home/Projects/Projects.jsx
@@ -1,11 +1,11 @@
 import TitleCaption from "../../../Hooks/TitleCaption";
-import projects1 from "../../../assets/img/projects/12.jpg"
-import projects2 from "../../../assets/img/projects/14.jpg"
-import projects3 from "../../../assets/img/projects/15.jpg"
+import projects1 from "../../../assets/img/projects/15.jpg"
+import projects2 from "../../../assets/img/projects/13.jpg"
+import projects3 from "../../../assets/img/authentication1.png"
 
 
 const Projects = () => {
-    const btnPrime ={
+    const btnPrime = {
         backgroundColor: "#212428", // Fix the property name and add a semicolon
         border: "none",
         boxShadow: "4px 0px 10px rgba(240, 239, 239, 0.1)",
@@ -14,18 +14,19 @@ const Projects = () => {
 
     return (
         <div id="projects" className='border-b-2 border-[#191c20] '>
-      
+
             <TitleCaption subTitle={'Projects'} mainTitle={'My Projects'}></TitleCaption>
-            <div className="mt-14 grid md:grid lg:grid-cols-3 gap-10 mb-24">
+            <div className="mt-14 grid md:grid-cols-2 lg:grid-cols-3 gap-10 mb-24">
                 <div className="card w-96 bg-[#1E2125] text-[#f1f0f0] shadow-xl hover:bg-gradient-to-r from-[#1E2125] to-[#141416] hover:-translate-y-3  hover:shadow-xl duration-300">
                     <figure className="px-10 pt-10">
                         <img src={projects1} alt="Shoes" className="rounded-xl" />
                     </figure>
                     <div className="card-body ">
-                        <h2 className="card-title">Hotel Management System</h2>
-                        <p>You can easily find your desire room & can booking. </p>
-                        <div className="card-actions">
-                            <button className="btn-prime px-6 py-3 rounded-md"><a href="https://github.com/habibur-01/Assignment-11">Details</a></button>
+                        <h2 className="card-title text-justify">TaskZen</h2>
+                        <p>You can easily add your upcoming task. Which is make your time management more efficient.Also it send you notification about your todays work. </p>
+                        <div className="card-actions space-x-2">
+                            <button className="btn-prime px-6 py-3 rounded-md"><a href="https://github.com/habibur-01/Task-Management-Client-">Details</a></button>
+                            <button className="btn-prime px-6 py-3 rounded-md"><a href="https://taskmanagement-7403c.web.app/">Live</a></button>
                         </div>
                     </div>
                 </div>
@@ -34,10 +35,11 @@ const Projects = () => {
                         <img src={projects2} alt="Shoes" className="rounded-xl" />
                     </figure>
                     <div className="card-body ">
-                        <h2 className="card-title">Brand Shop </h2>
-                        <p>You can find world famous brands clothes all in one.</p>
-                        <div className="card-actions">
-                            <button className="btn-prime px-6 py-3 rounded-md"><a href="https://github.com/habibur-01/Brand-Shop">Details</a></button>
+                        <h2 className="card-title">Contest_Hunter </h2>
+                        <p>ContestHub website refer to an online platform dedicated to hosting various types of contests. It can arrange various type ofcontest.</p>
+                        <div className="card-actions space-x-2">
+                            <button className="btn-prime px-6 py-3 rounded-md"><a href="https://github.com/habibur-01/CustomHub_Client">Details</a></button>
+                            <button className="btn-prime px-6 py-3 rounded-md"><a href="https://contesthub-5c493.web.app/">Live</a></button>
                         </div>
                     </div>
                 </div>
@@ -46,14 +48,15 @@ const Projects = () => {
                         <img src={projects3} alt="Shoes" className="rounded-xl" />
                     </figure>
                     <div className="card-body ">
-                        <h2 className="card-title">Employee Management System</h2>
-                        <p>You can easily manage your manpower over your company.</p>
-                        <div className="card-actions">
-                            <button className="btn-prime px-6 py-3 rounded-md"><a href="https://github.com/habibur-01/Employee_mangament">Details</a></button>
+                        <h2 className="card-title">Food-Cart</h2>
+                        <p>Food-Cart refers to an online platform dedicated to showcasing and possibly selling food items that are prepared and served from a restuarent.</p>
+                        <div className="card-actions space-x-2">
+                            <button className="btn-prime px-6 py-3 rounded-md"><a href="https://github.com/habibur-01/bistro_boss_client">Details</a></button>
+                            <button className="btn-prime px-6 py-3 rounded-md"><a href="https://bistro-boss-b68c7.web.app/">Live</a></button>
                         </div>
                     </div>
                 </div>
-                
+
             </div>
 
         </div>
